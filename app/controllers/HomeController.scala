5e8d39602f31d14045af470767cba186bb12a98d
Burak Ongün <50797633+burakong@users.noreply.github.com>
2021-05-08 14:52:23 +0200

    add database configuration

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -21,4 +21,8 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
+  def env() = Action { implicit request: Request[AnyContent] =>
+    //Ok("Nothing to see here")
+    Ok(System.getenv("JDBC_DATABASE_URL"))
+  }
 }
